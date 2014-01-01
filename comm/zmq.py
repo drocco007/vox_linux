e4c2723f547633b838559407e873417043c5fee6
Daniel Rocco <drocco@gmail.com>
2014-01-01 11:52:12 -0500

    zmq: dispose socket and context on exit

--- comm/zmq.py
+++ comm/zmq.py
@@ -1,7 +1,10 @@
 ﻿import clr
 
 clr.AddReferenceToFile("clrzmq.dll")
+
+from System import AppDomain
 from System.Text import Encoding
+
 import ZMQ as zmq
 from ZMQ import SocketType
 
@@ -19,14 +22,28 @@ socket.Bind("tcp://*:5556")
 #    message = socket.Recv(Encoding.ASCII)
 #    print "Received reply ", request, "[", message, "]"
 
+
 def send_text(text):
 	pass
+
 
 def send_key(key):
 #	print key, type(key), hex(unicode(key).encode('utf-8'))
 	socket.Send(key, Encoding.UTF8)
-	
+
 
 def send_command(text):
 	print 'command', text
 	send_key('\x02' + text)
+
+
+# clean up on process exit
+#
+# http://stackoverflow.com/a/2555354
+
+def cleanup(*args):
+	send_key('\x01Bridge exiting')
+	socket.Dispose()
+	context.Dispose()
+
+AppDomain.CurrentDomain.ProcessExit += cleanup
