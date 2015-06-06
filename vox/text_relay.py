91f383588dc262c2e87b60339bbdf9ee07df6c31
Daniel Rocco <drocco@gmail.com>
2015-06-05 21:33:26 -0400

    Replace python-uinput with evdev
    Probably voodoo. Attempting to eliminate intermittent dropped key
    sequences. I know the sequences are getting from Windows to Linux, so
    I suspect that there is something with the synthetic keyboard driver
    (buffer overrun or whatever) that causes it to drop events.
    Unfortunately I can't find any logging to pin down the root cause.

--- vox/text_relay.py
+++ vox/text_relay.py
@@ -1,115 +1,115 @@
 # coding: utf-8
 
-import uinput
+from evdev import UInput, ecodes
 
 import bus
 from commands import PRESS_KEY, PLAY_TEXT
 from process_utils import spawn_daemon_process
 
 
-UNICODE_PREFIX = [uinput.KEY_RIGHTALT, uinput.KEY_LEFTCTRL,
-                  uinput.KEY_LEFTSHIFT, uinput.KEY_U]
+UNICODE_PREFIX = [ecodes.KEY_RIGHTALT, ecodes.KEY_LEFTCTRL,
+                  ecodes.KEY_LEFTSHIFT, ecodes.KEY_U]
 
 modifier_map = {
-    'a': uinput.KEY_RIGHTALT,
-    'c': uinput.KEY_LEFTCTRL,
-    's': uinput.KEY_LEFTSHIFT,
-    'w': uinput.KEY_LEFTMETA,
+    'a': ecodes.KEY_RIGHTALT,
+    'c': ecodes.KEY_LEFTCTRL,
+    's': ecodes.KEY_LEFTSHIFT,
+    'w': ecodes.KEY_LEFTMETA,
 }
 
 
 key_map = {
-    'a': uinput.KEY_A,
-    'b': uinput.KEY_B,
-    'c': uinput.KEY_C,
-    'd': uinput.KEY_D,
-    'e': uinput.KEY_E,
-    'f': uinput.KEY_F,
-    'g': uinput.KEY_G,
-    'h': uinput.KEY_H,
-    'i': uinput.KEY_I,
-    'j': uinput.KEY_J,
-    'k': uinput.KEY_K,
-    'l': uinput.KEY_L,
-    'm': uinput.KEY_M,
-    'n': uinput.KEY_N,
-    'o': uinput.KEY_O,
-    'p': uinput.KEY_P,
-    'q': uinput.KEY_Q,
-    'r': uinput.KEY_R,
-    's': uinput.KEY_S,
-    't': uinput.KEY_T,
-    'u': uinput.KEY_U,
-    'v': uinput.KEY_V,
-    'w': uinput.KEY_W,
-    'x': uinput.KEY_X,
-    'y': uinput.KEY_Y,
-    'z': uinput.KEY_Z,
-    '1': uinput.KEY_1,
-    '2': uinput.KEY_2,
-    '3': uinput.KEY_3,
-    '4': uinput.KEY_4,
-    '5': uinput.KEY_5,
-    '6': uinput.KEY_6,
-    '7': uinput.KEY_7,
-    '8': uinput.KEY_8,
-    '9': uinput.KEY_9,
-    '0': uinput.KEY_0,
+    'a': ecodes.KEY_A,
+    'b': ecodes.KEY_B,
+    'c': ecodes.KEY_C,
+    'd': ecodes.KEY_D,
+    'e': ecodes.KEY_E,
+    'f': ecodes.KEY_F,
+    'g': ecodes.KEY_G,
+    'h': ecodes.KEY_H,
+    'i': ecodes.KEY_I,
+    'j': ecodes.KEY_J,
+    'k': ecodes.KEY_K,
+    'l': ecodes.KEY_L,
+    'm': ecodes.KEY_M,
+    'n': ecodes.KEY_N,
+    'o': ecodes.KEY_O,
+    'p': ecodes.KEY_P,
+    'q': ecodes.KEY_Q,
+    'r': ecodes.KEY_R,
+    's': ecodes.KEY_S,
+    't': ecodes.KEY_T,
+    'u': ecodes.KEY_U,
+    'v': ecodes.KEY_V,
+    'w': ecodes.KEY_W,
+    'x': ecodes.KEY_X,
+    'y': ecodes.KEY_Y,
+    'z': ecodes.KEY_Z,
+    '1': ecodes.KEY_1,
+    '2': ecodes.KEY_2,
+    '3': ecodes.KEY_3,
+    '4': ecodes.KEY_4,
+    '5': ecodes.KEY_5,
+    '6': ecodes.KEY_6,
+    '7': ecodes.KEY_7,
+    '8': ecodes.KEY_8,
+    '9': ecodes.KEY_9,
+    '0': ecodes.KEY_0,
 
-    '\t': uinput.KEY_TAB,
-    'Tab': uinput.KEY_TAB,
-    '\n': uinput.KEY_ENTER,
-    'Return': uinput.KEY_ENTER,
-    ' ': uinput.KEY_SPACE,
-    'space': uinput.KEY_SPACE,
-    'Delete': uinput.KEY_DELETE,
-    'Escape': uinput.KEY_ESC,
-    'BackSpace': uinput.KEY_BACKSPACE,
+    '\t': ecodes.KEY_TAB,
+    'Tab': ecodes.KEY_TAB,
+    '\n': ecodes.KEY_ENTER,
+    'Return': ecodes.KEY_ENTER,
+    ' ': ecodes.KEY_SPACE,
+    'space': ecodes.KEY_SPACE,
+    'Delete': ecodes.KEY_DELETE,
+    'Escape': ecodes.KEY_ESC,
+    'BackSpace': ecodes.KEY_BACKSPACE,
 
-    '.': uinput.KEY_DOT,
-    ',': uinput.KEY_COMMA,
-    '/': uinput.KEY_SLASH,
-    '\\': uinput.KEY_BACKSLASH,
-    "'": uinput.KEY_APOSTROPHE,
-    ';': uinput.KEY_SEMICOLON,
-    '[': uinput.KEY_LEFTBRACE,
-    ']': uinput.KEY_RIGHTBRACE,
-    '-': uinput.KEY_MINUS,
-    'minus': uinput.KEY_MINUS,
-    '=': uinput.KEY_EQUAL,
-    '~': uinput.KEY_GRAVE,
+    '.': ecodes.KEY_DOT,
+    ',': ecodes.KEY_COMMA,
+    '/': ecodes.KEY_SLASH,
+    '\\': ecodes.KEY_BACKSLASH,
+    "'": ecodes.KEY_APOSTROPHE,
+    ';': ecodes.KEY_SEMICOLON,
+    '[': ecodes.KEY_LEFTBRACE,
+    ']': ecodes.KEY_RIGHTBRACE,
+    '-': ecodes.KEY_MINUS,
+    'minus': ecodes.KEY_MINUS,
+    '=': ecodes.KEY_EQUAL,
+    '~': ecodes.KEY_GRAVE,
 
-    'Left': uinput.KEY_LEFT,
-    'Right': uinput.KEY_RIGHT,
-    'Up': uinput.KEY_UP,
-    'Down': uinput.KEY_DOWN,
-    'Home': uinput.KEY_HOME,
-    'End': uinput.KEY_END,
-    'Page_Up': uinput.KEY_PAGEUP,
-    'Page_Down': uinput.KEY_PAGEDOWN,
-    'Insert': uinput.KEY_INSERT,
+    'Left': ecodes.KEY_LEFT,
+    'Right': ecodes.KEY_RIGHT,
+    'Up': ecodes.KEY_UP,
+    'Down': ecodes.KEY_DOWN,
+    'Home': ecodes.KEY_HOME,
+    'End': ecodes.KEY_END,
+    'Page_Up': ecodes.KEY_PAGEUP,
+    'Page_Down': ecodes.KEY_PAGEDOWN,
+    'Insert': ecodes.KEY_INSERT,
 
-    'F1': uinput.KEY_F1,
-    'F2': uinput.KEY_F2,
-    'F3': uinput.KEY_F3,
-    'F4': uinput.KEY_F4,
-    'F5': uinput.KEY_F5,
-    'F6': uinput.KEY_F6,
-    'F7': uinput.KEY_F7,
-    'F8': uinput.KEY_F8,
-    'F9': uinput.KEY_F9,
-    'F10': uinput.KEY_F10,
-    'F11': uinput.KEY_F11,
-    'F12': uinput.KEY_F12,
+    'F1': ecodes.KEY_F1,
+    'F2': ecodes.KEY_F2,
+    'F3': ecodes.KEY_F3,
+    'F4': ecodes.KEY_F4,
+    'F5': ecodes.KEY_F5,
+    'F6': ecodes.KEY_F6,
+    'F7': ecodes.KEY_F7,
+    'F8': ecodes.KEY_F8,
+    'F9': ecodes.KEY_F9,
+    'F10': ecodes.KEY_F10,
+    'F11': ecodes.KEY_F11,
+    'F12': ecodes.KEY_F12,
 }
 
 
 chord_map = {
-    ':': [uinput.KEY_LEFTSHIFT, uinput.KEY_SEMICOLON],
-    '?': [uinput.KEY_LEFTSHIFT, uinput.KEY_SLASH],
-    '"': [uinput.KEY_LEFTSHIFT, uinput.KEY_APOSTROPHE],
-    '+': [uinput.KEY_LEFTSHIFT, uinput.KEY_EQUAL],
-    '_': [uinput.KEY_LEFTSHIFT, uinput.KEY_MINUS],
+    ':': [ecodes.KEY_LEFTSHIFT, ecodes.KEY_SEMICOLON],
+    '?': [ecodes.KEY_LEFTSHIFT, ecodes.KEY_SLASH],
+    '"': [ecodes.KEY_LEFTSHIFT, ecodes.KEY_APOSTROPHE],
+    '+': [ecodes.KEY_LEFTSHIFT, ecodes.KEY_EQUAL],
+    '_': [ecodes.KEY_LEFTSHIFT, ecodes.KEY_MINUS],
 }
 
 
@@ -121,6 +121,17 @@ def unpack_keycodes(command):
         modifiers = []
 
     return modifiers + [key_map.get(command, command)]
+
+
+def emit_combo(kbd, keys, syn=False):
+    for key in keys:
+        kbd.write(ecodes.EV_KEY, key, 1)
+
+    for key in reversed(keys):
+        kbd.write(ecodes.EV_KEY, key, 0)
+
+    if syn:
+        kbd.syn()
 
 
 def emit_text(kbd, text):
@@ -139,15 +150,15 @@ def emit_text(kbd, text):
 
     for char in text:
         if char in chord_map:
-            kbd.emit_combo(chord_map[char], syn=True)
+            emit_combo(kbd, chord_map[char], syn=True)
         elif char.isupper():
-            chord = [uinput.KEY_LEFTSHIFT, key_map[char.lower()]]
-            kbd.emit_combo(chord, syn=True)
+            chord = [ecodes.KEY_LEFTSHIFT, key_map[char.lower()]]
+            emit_combo(kbd, chord, syn=True)
         elif char in key_map:
-            kbd.emit_click(key_map[char], syn=True)
+            emit_combo(kbd, [key_map[char]], syn=True)
         else:
             codepoint = hex(ord(char))[2:] + u' '
-            kbd.emit_combo(UNICODE_PREFIX, syn=True)
+            emit_combo(kbd, UNICODE_PREFIX, syn=True)
             emit_text(kbd, codepoint)
 
 
@@ -155,10 +166,10 @@ def relay_text_worker(host='localhost'):
     socket = bus.connect_subscribe(host=host,
                                    subscriptions=(PRESS_KEY, PLAY_TEXT))
 
-    all_keys = [v for k,v in uinput.ev.__dict__.items()
-                if k.startswith('KEY_')]
+    # all_keys = [v for k,v in uinput.ev.__dict__.items()
+                # if k.startswith('KEY_')]
 
-    with uinput.Device(all_keys) as kbd:
+    with UInput() as kbd:
         while True:
             message = socket.recv()
 
@@ -169,12 +180,14 @@ def relay_text_worker(host='localhost'):
                     keycodes = unpack_keycodes(message)
 
                     print '.key:', message, '→', keycodes
-                    kbd.emit_combo(keycodes)
+                    emit_combo(kbd, keycodes)
                 else:
                     print '.text: {} ({})'.format(message, type(message))
                     emit_text(kbd, unicode(message))
             except ValueError as e:
                 print u'Error processing message: {}'.format(message)
+
+            kbd.syn()
 
 
 def relay_text(host='localhost'):
